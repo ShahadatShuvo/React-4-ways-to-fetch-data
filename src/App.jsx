0ad63960ab5efc7e2ca4e780517c26ff519e11d1
ShahadatShuvo <hossainshuvo7860@gmail.com>
2022-08-03 20:52:45 +0600

    swr implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Quote from "./components/axios/Quote";
+import Quote from "./components/swr/Quote";
 
 function App() {
   return (
---
+++ src/components/swr/Quote.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import axios from "axios";
+import useSWR from "swr";
+
+const fetchQuote = async (...args) => {
+  const res = await axios(...args);
+  return res.data;
+};
+
+function Quote() {
+  const { data, error } = useSWR(
+    "https://jsonplaceholder.typicode.com/users",
+    fetchQuote
+  );
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
+  return <div>Axios-:Quote title: {data[0]?.name}</div>;
+}
+
+export default Quote;
